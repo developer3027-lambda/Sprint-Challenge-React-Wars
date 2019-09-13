314258b11d3e366cf5e15cae0a4efce07ddd896e
Mason Roberts <mrobe18@gmail.com>
2019-09-13 12:15:59 -0400

    recieved msg from SWinfo on browser

--- starwars/src/components/SWinfo.js
+++ starwars/src/components/SWinfo.js
@@ -1,4 +1,11 @@
 import React, {useState, useEffect} from 'react';
-import axios from 'axios';
-import SWcard from './SWcard';
+// import axios from 'axios';
+// import SWcard from './SWcard';
 
+function SWinfo() {
+
+    return (
+        <h1>Saying Hi</h1>
+    )
+}
+export default SWinfo;
