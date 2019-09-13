bd7fcf9cbfaf8adce919958e217ea99384005184
Mason Roberts <mrobe18@gmail.com>
2019-09-13 12:35:26 -0400

    got the info from api

--- starwars/src/components/SWinfo.js
+++ starwars/src/components/SWinfo.js
@@ -1,8 +1,23 @@
 import React, {useState, useEffect} from 'react';
-// import axios from 'axios';
+import axios from 'axios';
 // import SWcard from './SWcard';
 
 function SWinfo() {
+
+    const [info, setInfo] = useState([]);
+
+    useEffect(() => {
+        axios
+        .get('https://swapi.co/api/people/')
+        .then(response => {
+            const info = response.data;
+            setInfo(info);
+            console.log(info);
+        })
+        .catch(error => {
+            console.log('caught from response: ', error);
+        })
+    },[])
 
     return (
         <h1>Saying Hi</h1>
